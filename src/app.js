a46756a93f15f105e4db5af3e88cbb59e8545bff
Jason Taisey <jdtaisey89@icloud.com>
2021-06-03 21:06:34 -0700

    refactored List

--- src/app.js
+++ src/app.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
 import Todo from './components/todo/todo.js'
-// import './app.css'
 
 export default function App() {
     return (
       <>
-      <Todo />
+        <Todo />
       </>
     )
 }
-// export default class App extends React.Component {
-//   render () {
-//     return (
-//       <>
-//       <Todo />
-//       </>
-//     )
-//   }
-// }
